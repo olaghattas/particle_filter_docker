c8f4983ef5c046720578779727eae5396d01eb2d
olaghattas <olaghattas@hotmail.com>
2024-06-05 20:13:10 -0400

    update

--- launch_pf.bash
+++ launch_pf.bash
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 source /home/particle_filter/install/setup.bash
+ros2 launch aptags_tf_broadcast tf_broadcast_helper.launch.py &
+
+sleep 10s
 
 sleep 1
 ros2 run particle_filter_mesh particle_filter_node &
